95e7d9802535bf0391992d07d33b1f7bf75946a4
Amin Jamali <ajamali@vmware.com>
2023-03-16 15:12:22 +0000

    Bump to lager v3 import path

--- cf_debug_server_test.go
+++ cf_debug_server_test.go
@@ -9,7 +9,7 @@ import (
 	"strconv"
 
 	cf_debug_server "code.cloudfoundry.org/debugserver"
-	"code.cloudfoundry.org/lager"
+	"code.cloudfoundry.org/lager/v3"
 	"github.com/tedsuo/ifrit"
 	"github.com/tedsuo/ifrit/ginkgomon"
 
